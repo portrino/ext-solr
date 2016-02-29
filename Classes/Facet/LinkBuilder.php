516d45e696f1548187d1ab0bf0d2ea1e8aaf19aa
Thomas Grießbach <griessbach@portrino.de>
2016-02-29 15:44:41 +0100

    updates Facet/LinkBuilder.php: allow special case if TS option "faceting.singleFacetMode = 1" && "facet.singleOptionMode = 1"

--- Classes/Facet/LinkBuilder.php
+++ Classes/Facet/LinkBuilder.php
@@ -268,6 +268,12 @@ class LinkBuilder extends Query\LinkBuilder
             // move facet to the end of the uri so it may be manipulated using JavaScript
             unset($filterParameters[$indexToReplace]);
             $filterParameters[$indexToReplace] = $this->facetName . ':' . $this->facetOption->getUrlValue();
+
+        } else if ($this->solrConfiguration['search.']['faceting.']['singleFacetMode']) {
+            // special case if TS option "faceting.singleFacetMode = 1" && "facet.singleOptionMode = 1"
+            // kill other parameters and use only current facet
+            $filterParameters = array($this->facetName . ':' . $this->facetOption->getUrlValue());
+            
         } else {
             // facet not found, add facet
             $filterParameters[] = $this->facetName . ':' . $this->facetOption->getUrlValue();
